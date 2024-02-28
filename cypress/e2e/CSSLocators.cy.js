b63e495e1492e24f4c28dbdabb830c5a93961ee1
Babar1998 <babararifmasood98@gmail.com>
2024-02-28 14:51:04 +0500

    Automation Learning

--- cypress/e2e/CSSLocators.cy.js
+++ cypress/e2e/CSSLocators.cy.js
@@ -6,9 +6,11 @@ describe('CSSLocators', () => {
         cy.get("[name='email']").type("babar@gmail.com")
         cy.get("[name='password']").type("123456")
         cy.get("[type='submit']").click()
-        cy.get(".nav-item").contains("Logout")
+        cy.get(".nav-item").contains("Products").click()
+        cy.get("[type='search']").type("Acer")
         cy.wait(10000)
         cy.get(".nav-item").contains("Logout").click()
+        
     })
 
   })
