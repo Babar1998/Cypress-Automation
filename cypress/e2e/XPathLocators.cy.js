8e3c5b178749754d5544b6fe9e1076e5ce1891c6
Babar Arif <babararifmasood98@gmail.com>
2024-03-09 01:20:19 +0500

    Learning

--- cypress/e2e/XPathLocators.cy.js
+++ cypress/e2e/XPathLocators.cy.js
@@ -4,15 +4,15 @@ describe('XPATHLocators', () => {
     it('no. of products', () => {
         cy.visit("https://reactjs-ecommerce-app.vercel.app/product")
         cy.wait(10000)
-        cy.xpath("//div[@class='col-md-4 col-sm-6 col-xs-8 col-12 mb-4']").should('have.length', 20)
+        cy.xpath("(//div[@class='card text-center h-100'])//img").should('have.length', 20)
         
     })
 
-    // it('chained xpaths', () => {
-    //     cy.visit("http://localhost:5173/products")
-    //     cy.wait(10000)
-    //     cy.xpath("//div[contains(@class,'bg-white h-100vh')]").xpath(".//a").should('have.length', 20)
+    it('chained xpaths', () => {
+        cy.visit("https://reactjs-ecommerce-app.vercel.app/product")
+        cy.wait(10000)
+        cy.xpath("(//div[@class='card text-center h-100'])").xpath(".//p").should('have.length', 20)
         
-    // })
+    })
 
   })
