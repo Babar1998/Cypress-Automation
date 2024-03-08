9f1948ec699426105635fbd93df4134d1154e56c
Babar1998 <babararifmasood98@gmail.com>
2024-03-08 17:26:46 +0500

    Assertions

---
+++ cypress/e2e/Assertions.cy.js
@@ -0,0 +1,12 @@
+
+describe('Assertions Demo', () => {
+
+    it('Implicit Assertions', () => {
+        cy.visit("http://localhost:5173/products")
+        cy.url().should('include', 'localhost:5173')
+        cy.url().should('eq', 'http://localhost:5173/products')
+        cy.url().should('contain', 'products')
+        
+    })
+
+  })
