a88c047514a5f3ffcd40e0dcb046749abe026b15
Babar Arif <babararifmasood98@gmail.com>
2024-03-09 14:32:21 +0500

    Implicit Assertions

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -2,10 +2,10 @@
 describe('Assertions Demo', () => {
 
     it('Implicit Assertions', () => {
-        cy.visit("https://www.daraz.pk/shop/xiaomi/?spm=a2a0e.home.search.9.35e34076twkD18&tab=allproducts&_keyori=ss&from=search_history&sugg=Xiaomi%20Flagship%20Store_8_1")
-        cy.url().should('include', 'daraz.pk')
-        cy.url().should('eq', 'https://www.daraz.pk/shop/xiaomi/?spm=a2a0e.home.search.9.35e34076twkD18&tab=allproducts&_keyori=ss&from=search_history&sugg=Xiaomi%20Flagship%20Store_8_1')
-        cy.url().should('contain', 'xiaomi')
+        cy.visit("https://reactjs-ecommerce-app.vercel.app/product")
+        cy.url().should('include', 'ecommerce')
+        cy.url().should('eq', 'https://reactjs-ecommerce-app.vercel.app/product')
+        cy.url().should('contain', 'product')
         
     })
 
