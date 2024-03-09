464eb0c91035b55a9ccd3f76fc3add0fab21c496
Babar Arif <babararifmasood98@gmail.com>
2024-03-10 01:23:52 +0500

    Implicit Assertions

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -2,12 +2,20 @@
 describe('Assertions Demo', () => {
 
     it('Implicit Assertions', () => {
-        cy.visit("https://reactjs-ecommerce-app.vercel.app/product")
-        cy.url().should('include', 'ecommerce')
-        cy.url().should('eq', 'https://reactjs-ecommerce-app.vercel.app/product')
-        cy.url().should('contain', 'product')
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.url().should('include', 'orangehrmlive.com')
+        .and('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        .and('contain', 'orangehrm')
         
     })
+
+    // it('Explicit Assertions', () => {
+    //     cy.visit("https://reactjs-ecommerce-app.vercel.app/product")
+    //     cy.url().should('include', 'ecommerce')
+    //     cy.url().should('eq', 'https://reactjs-ecommerce-app.vercel.app/product')
+    //     cy.url().should('contain', 'product')
+        
+    // })
 
   })
 
