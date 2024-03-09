95ef5a4ade25e5bd978871a1085a108e2daa9d39
Babar Arif <babararifmasood98@gmail.com>
2024-03-10 01:44:42 +0500

    Implicit Assertions

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -2,11 +2,21 @@
 describe('Assertions Demo', () => {
 
     it('Implicit Assertions', () => {
-        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
         cy.url().should('include', 'orangehrmlive.com')
         .and('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
         .and('contain', 'orangehrm')
+        .and('not.contain', 'greenhrm')
+
+        cy.title().should('include', 'Orange')
+        .and('eq', 'OrangeHRM')
+        .and('contain', 'HRM')
         
+        
+        cy.get('.orangehrm-login-branding > img').should('be.visible')
+        .and('exist')
+
+        cy.xpath('//a').should('have.length', '5')
     })
 
     // it('Explicit Assertions', () => {
