7fa9022dbddacafc02894348c4200e7c3a321c87
Babar Arif <babararifmasood98@gmail.com>
2024-03-15 21:12:21 +0500

    Learning

--- cypress/e2e/MyFirstTest.cy.js
+++ cypress/e2e/MyFirstTest.cy.js
@@ -8,7 +8,7 @@ describe('My First Test', () => {
 
     it('verify title-negative', () => {
         cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-        cy.title().should('eq','OrangeHRM123')
+        cy.title().should('not.eq','OrangeHRM123')
     })
 
   })
