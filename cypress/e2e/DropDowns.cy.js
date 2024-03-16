767d6735ec5bd792900b9f13915bfcb3113b6f93
Babar Arif <babararifmasood98@gmail.com>
2024-03-16 20:33:50 +0500

    dropdowns

---
+++ cypress/e2e/DropDowns.cy.js
@@ -0,0 +1,36 @@
+/// <reference types ="Cypress" />
+describe('handle dropdowns', () => {
+
+    it.skip('Dropdown with select', () => {
+        cy.visit("https://testautomationpractice.blogspot.com/")
+        cy.get('#country')
+        .select('Canada')
+        .should('have.value', 'canada')
+        
+    })
+
+    it.skip('Dropdown with select', () => {
+        cy.visit("https://www.zoho.com/commerce/free-demo.html")
+        cy.get('#zcf_address_country').should('be.visible')
+        .select('Pakistan')
+        .should('have.value', 'Pakistan')
+        
+    })
+
+    it.skip('Dropdown without select', () => {
+        cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
+        cy.get('#select2-billing_country-container').click()
+        cy.get('.select2-search__field').type('United Arab Emirates').type('{enter}')
+        cy.get('#select2-billing_country-container')
+        .should('have.text', 'United Arab Emirates')
+        
+    })
+
+    it('Auto Suggest Dropdown', () => {
+        cy.visit("https://www.wikipedia.org/")
+        cy.get('#searchInput').type('Pakistan')
+        
+        
+    })
+
+})
