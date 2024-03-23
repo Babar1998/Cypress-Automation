3dcf461c31fa5c690fe5a5cb52044239739eb1dd
Babar Arif <babararifmasood98@gmail.com>
2024-03-24 02:21:17 +0500

    Alerts

--- cypress/e2e/Alerts.cy.js
+++ cypress/e2e/Alerts.cy.js
@@ -1,18 +1,18 @@
 /// <reference types ="Cypress" />
 describe('Alerts', () => {
 
-    it('Alert with text & OK', () => {
+    it('Javascript Alert with text & OK', () => {
         cy.visit("https://testautomationpractice.blogspot.com/")
         
         
     })
 
-    it('Alert with text, OK & Cancel', () => {
+    it('Javascript Confirm Alert with text, OK & Cancel', () => {
         cy.visit("https://testautomationpractice.blogspot.com/")
         
         
     })
-    it('Alert with User Input & OK', () => {
+    it('Javascript Prompt Alert with User Input & OK', () => {
         cy.visit("https://testautomationpractice.blogspot.com/")
         
         
