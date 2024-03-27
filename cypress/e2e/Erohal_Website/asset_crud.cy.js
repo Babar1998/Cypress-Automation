198714611c1f7c0bb61e775e1e493f385313751e
Babar Arif <babararifmasood98@gmail.com>
2024-03-27 23:09:32 +0500

    Asset Read

--- cypress/e2e/Erohal_Website/asset_crud.cy.js
+++ cypress/e2e/Erohal_Website/asset_crud.cy.js
@@ -6,6 +6,9 @@ describe('Asset CRUD', () => {
         cy.get("input[type='email']").type("erohaladmin@gmail.com")
         cy.get("input[type='password']").type("erohal")
         cy.get(".btn-text").click()
+        cy.get('[for="menu"] > .v-icon').click()
+        cy.get(':nth-child(8) > .posRelA > .toggle-menu > .v-icon').click()
+        cy.get(':nth-child(8) > .menu-dropdown > :nth-child(1) > a').click()
     })
 
     it('Asset `Creation', () => {
